5f38a651f946fec2dc27d7a0eb56d44f4a64fd61
youmikai <tourokuma4@gmail.com>
2018-11-14 19:49:29 +0900

    Unity-ScriptTest
    mp5でも攻撃実行できるように更新

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -25,7 +25,7 @@ public class Boss
     public void Magic()
     {
         
-        if (5<mp)
+        if (5<=mp)
         {
             this.mp -= 5;
             Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
